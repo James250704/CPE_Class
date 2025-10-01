3394eb76c90c995a508b589bcb633d63ddbb14f7
James250704 <James20050704@gmail.com>
2025-10-01 16:37:21 +0800

    新增練習題目

---
+++ Test/10041.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long long func(vector<long long> &arr) {
+    sort(arr.begin(), arr.end());
+    long long result = 0;
+    long long vito = arr.size() / 2;
+    for (size_t i = 0; i < arr.size(); i++) {
+        result += abs(arr[vito] - arr[i]);
+    }
+    return result;
+}
+
+int main() {
+    long long n;
+    cin >> n;
+    while (n--) {
+        long long round;
+        cin >> round;
+        vector<long long> arr(round);
+        for (long long i = 0; i < round; i++) {
+            cin >> arr[i];
+        }
+        cout << func(arr) << endl;
+    }
+
+    return 0;
+}
---
+++ Test/10929.cpp
@@ -0,0 +1,47 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool func(string a) {
+    string odd;
+    string even;
+    for (long long i = 0; i < a.length(); i++) {
+        int digit = a[i] - '0';
+        if (i % 2 != 0) {
+            odd.push_back(digit);
+        } else {
+            even.push_back(digit);
+        }
+    }
+    long long oddtemp = 0;
+    long long eventemp = 0;
+    for (long long i = 0; i < odd.length(); i++) {
+        oddtemp += odd[i];
+    }
+    for (long long i = 0; i < even.length(); i++) {
+        eventemp += even[i];
+    }
+
+    int temp = abs(oddtemp - eventemp);
+    if (temp % 11 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    string a;
+    while (cin >> a) {
+        if (a == "0") {
+            break;
+        }
+        if (func(a)) {
+            cout << a << " is a multiple of 11." << endl;
+        } else {
+            cout << a << " is not a multiple of 11." << endl;
+        }
+    }
+    return 0;
+}
