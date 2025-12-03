fbab46b4f604c81793754e0c62926f22bf6fd91f
James250704 <James20050704@gmail.com>
2025-12-03 11:57:04 +0800

    更新quiz3

--- Quiz03/10921.cpp
+++ Quiz03/10921.cpp
@@ -1,18 +1,68 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
     string phone;
-    string result;
     while(cin >> phone){
+        string result;
+        int count = 0, count2 = 0;
         for(int i = 0; i < phone.size(); i++){
-            if(phone[i] == 1 || phone[i] == 0){
+            if(phone[i] == '1' || phone[i] == '0' || phone[i] == '-'){
                 result += phone[i];
+                if(phone[i] == '-') {
+                    count++;
+                }
             }else{
-                cout << char(phone[i])<< endl;
+                count2++;
+                switch(phone[i]){
+                    case 'A':
+                    case 'B':
+                    case 'C':
+                     result += "2";
+                     break;
+                    case 'D':
+                    case 'E':
+                    case 'F':
+                     result += "3";
+                     break;
+                    case 'G':
+                    case 'H':
+                    case 'I':
+                     result += "4";
+                     break;
+                     case 'J':
+                    case 'K':
+                    case 'L':
+                     result += "5";
+                     break;
+                     case 'M':
+                    case 'N':
+                    case 'O':
+                     result += "6";
+                     break;
+                     case 'P':
+                    case 'Q':
+                    case 'R':
+                    case 'S':
+                     result += "7";
+                     break;
+                     case 'T':
+                    case 'U':
+                    case 'V':
+                     result += "8";
+                     break;
+                     case 'W':
+                    case 'X':
+                    case 'Y':
+                    case 'Z':
+                     result += "9";
+                     break;
+                }
             }
             
         }
+        cout << result << " " << count2 << " " << count << endl;
     }
     return 0;
 }
--- Quiz03/12019.cpp
+++ Quiz03/12019.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    cin >> n;
+    while(n--){
+        int month, day;
+        cin >> month >> day;
+
+        
+    }
     return 0;
 }
